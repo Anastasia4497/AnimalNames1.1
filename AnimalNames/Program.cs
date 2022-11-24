2552e6291096494b47f6bce4437473dfb2ca4c47
Анастасия Боговид <anastaciabogovid@yandex.ru>
2022-11-25 00:02:20 +0300

    AnimalNames

--- AnimalNames/Program.cs
+++ AnimalNames/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            name = ("Введите имена животных: Лямбда, Бакс, Принцесса");
+            Console.WriteLine(name);
+            Console.ReadLine();
+
         }
     }
-}
+    }
